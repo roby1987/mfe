b2248944f3102efa9cf504fa038eb01459faab63
ARDANTE ROBERTA (DTO) <ARDANTER@posteitaliane.it>
2022-09-21 15:39:10 +0200

    update2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,25 +1,12 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from "@material-ui/core/styles";
-import MarketingApp from "../components/MarketingApp";
-import Header from "../components/Header";
-
-const generateClassName = createGenerateClassName({
-  productionPrefix: "co",
-});
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-    <BrowserRouter>
-      <StylesProvider generateClassName={generateClassName}>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
-      </StylesProvider>
-    </BrowserRouter>
+    <div>
+      <h1>Hi there!!!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
   );
 };
