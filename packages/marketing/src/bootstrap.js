a9a4b992e05ee2a78fbe5e90564e1d574b67b102
ARDANTE ROBERTA (DTO) <ARDANTER@posteitaliane.it>
2022-09-21 11:59:48 +0200

    randomize production classnames

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -18,10 +18,3 @@ if (process.env.NODE_ENV === "development") {
 //We are running through container
 //and we should export the mount function
 export { mount };
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("_marketing-dev-root")
-// );
