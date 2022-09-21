18d67bb615df3c1884021d5459f2b1356c8fba63
ARDANTE ROBERTA (DTO) <ARDANTER@posteitaliane.it>
2022-09-21 14:19:08 +0200

    randomize production classnames

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-//Mount function to start up the app
+// Mount function to start up the app
 const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
-//If we are in development and in isolation,
-//call mount immediately
-if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
     mount(devRoot);
   }
 }
-//We are running through container
-//and we should export the mount function
+
+// We are running through container
+// and we should export the mount function
 export { mount };
