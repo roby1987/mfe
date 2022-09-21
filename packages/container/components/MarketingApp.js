6bf340a221f48963eeccb976e8bb9d3c9eac2fd4
ARDANTE ROBERTA (DTO) <ARDANTER@posteitaliane.it>
2022-09-21 12:27:07 +0200

    randomize production classnames

--- packages/container/components/MarketingApp.js
+++ packages/container/components/MarketingApp.js
@@ -1,4 +1,4 @@
-import { React,  useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
 
 export default () => {
@@ -9,3 +9,4 @@ export default () => {
   });
   return <div ref={ref} />;
 };
+
